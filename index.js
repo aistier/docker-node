174eecad6d2496f1586809493bd342e98e38ec61
zozi@aistier.com <zozi@aistier.com>
2019-06-11 09:16:59 +0900

    docker-node start

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+ 
+app.listen(80, () => {
+  console.log('test listening on port 80');
+});
